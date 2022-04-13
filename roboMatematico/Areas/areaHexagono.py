eb34715ba63837ea5de081d2980d1659e24ed576
guifilho <98508021+guifilho@users.noreply.github.com>
2022-04-13 14:50:55 -0300

    Delete roboMatematico/Areas directory

--- roboMatematico/Areas/areaHexagono.py
+++
@@ -1,8 +0,0 @@
-# PARA PENTAGONOS DE LADOS IGUAIS #
-from math import sqrt
-
-ladoHex = float(input("Tamanho dos lados: "))
-
-areaFinal = (3 * (ladoHex*ladoHex) * sqrt(3)) / 2
-
-print("Area do hexagono 3 x {}² x raiz de 3 / 2 = {}m²".format(ladoHex,areaFinal))
