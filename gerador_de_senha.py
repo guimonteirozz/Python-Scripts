f8dfbd87ad91dae8a1a28ed5cbdd04819ad608a5
Guilherme Filho <98508021+guifilho@users.noreply.github.com>
2022-06-06 01:31:17 -0300

    Create gerador_de_senha.py

---
+++ gerador_de_senha.py
@@ -0,0 +1,15 @@
+lette_upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+lette_lower = 'abcdefghijklmnopqrstuvwxyz'
+numbers = '0123456789'
+symbols = '@#$%&*_+=!,.'
+
+total = lette_upper + lette_lower + numbers + symbols
+
+numero_caracteres = int(input('Numero de Caracteres da senha: '))
+
+while numero_caracteres < 8:
+    print('O NUMERO DE CARACTERES TEM QUE SER MAIOR OU IGUAL A 8')
+    numero_caracteres = int(input('Numero de Caracteres da senha: '))
+
+list_str = ','.join(choices(total, k=numero_caracteres))
+print(list_str.replace(',', ''))
