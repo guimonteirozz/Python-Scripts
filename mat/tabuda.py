4f394bec4ab05e372fd60bd45841626c30c5fc8b
Guilherme Filho <98508021+guifilho@users.noreply.github.com>
2022-10-16 12:48:14 -0300

    Update tabuda.py

--- mat/tabuda.py
+++ mat/tabuda.py
@@ -1,7 +1,7 @@
 valor = int(input("Qual numero deseja saber a tabuada: "))
 tamanho = int(input("Até onde vai sua tabuada: "))
 
-for tamanho in range(1,tamanho+1):
-    produto = tamanho*valor
+for tamanho in range(1, tamanho + 1):
+    produto = tamanho * valor
 
-    print("{} x {} = {}".format(tamanho,valor,produto))
+    print(f"{tamanho} x {valor} = {produto}")
