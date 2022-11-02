ceedc8c5834fe98813fe7e112ca9cab3d76d6830
Guilherme Filho <98508021+guifilho@users.noreply.github.com>
2022-11-02 19:19:39 -0300

    Update gerador_de_senha.py

--- gerador_de_senha.py
+++ gerador_de_senha.py
@@ -10,7 +10,7 @@ total = lette_upper + lette_lower + numbers + symbols
 numero_caracteres = int(input('Numero de Caracteres da senha: '))
 
 while numero_caracteres < 8:
-    print('O NUMERO DE CARACTERES TEM QUE SER MAIOR OU IGUAL A 8')
+    print('O NUMERO DE CARACTERES TEM QUE SER MAIOR OU IGUAL A 8 PARA UM SENHA FORTE')
     numero_caracteres = int(input('Numero de Caracteres da senha: '))
 
 list_str = ','.join(choices(total, k=numero_caracteres))
